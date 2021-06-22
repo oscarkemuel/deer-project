29a4b41a95125a49269d2f86ceb2a923c5f42ce2
Oscar Kemuel <oscar.kemuel5@gmail.com>
2021-06-22 00:53:24 -0300

    Section2 completed

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -1,12 +1,40 @@
-import { Container, Button } from '@material-ui/core';
-import { AiFillPlayCircle } from 'react-icons/ai';
+import { Container } from '@material-ui/core';
+import { FaChild } from 'react-icons/fa';
+import { GiPayMoney } from 'react-icons/gi';
+import CountUp from 'react-countup';
+import ScrollAnimation from 'react-animate-on-scroll';
 import styled from '../styles/components/Section2.module.scss';
+import 'animate.css/animate.min.css';
 
 const Section2: React.FC = () => {
   return (
     <section className={styled.section} id="">
       <Container className={styled.content}>
-        <h1>hello</h1>
+        <ScrollAnimation animateIn="bounceInLeft" duration={0.5}>
+          <div>
+            <FaChild />
+            <p className="number">
+              <CountUp end={30} duration={5} prefix="+" />
+            </p>
+            <p>crianças foram ajudadas a lidar com sua forma de ser</p>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="bounceInRight" duration={0.5}>
+          <div>
+            <GiPayMoney />
+            <p className="number">
+              <CountUp
+                end={450000}
+                duration={5}
+                prefix="+R$ "
+                decimals={2}
+                decimal=","
+              />
+            </p>
+            <p>já foram arrecadados para a causa</p>
+          </div>
+        </ScrollAnimation>
       </Container>
     </section>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,18 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap"
             rel="stylesheet"
           />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
+          />
         </Head>
         <body>
           <Main />
